92473493825ef065bb4bb8c22e85b327aa712924
Robert Stefanic <rstefanic72@gmail.com>
2018-10-11 09:26:06 -0500

    Added simple DB test to test suite

---
+++ GnuPlusLinuxTestSuite/DBTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Xunit;
+
+using GnuPlusLinuxDAL;
+
+namespace GnuPlusLinuxTestSuite
+{
+    public class DBTest
+    {
+		private static AccountContext _accountContext;
+
+		static DBTest() 
+		{
+			IConfiguration config = new ConfigurationBuilder().Build();
+
+			// Just use localDB for testing
+			string connectionString = 
+				"Server=(localdb)\\mssqllocaldb;Database=GNUPlusLinux;" +
+				"Trusted_Connection=True;MultipleActiveResultSets=true";
+
+			DbContextOptions<AccountContext> DbContextOptions = 
+				new DbContextOptionsBuilder<AccountContext>()
+					.UseSqlServer(connectionString)
+					.Options;
+
+			_accountContext = new AccountContext(DbContextOptions);
+		}
+
+		[Fact]
+		public void DatabaseIsSqlServer() 
+		{
+			Assert.True(_accountContext.Database.IsSqlServer());
+		}
+    }
+}
